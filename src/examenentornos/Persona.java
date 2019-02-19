3de2821c8ca164f3dc0a4976611aaa206cbf074c
Alumno <Alumno@AULA1-PC09>
2019-02-19 12:05:56 +0100

    Primer cambio

--- src/examenentornos/Persona.java
+++ src/examenentornos/Persona.java
@@ -1,12 +1,7 @@
 package examenentornos;
 /*Este codigo define la clase Persona y la clase Profesor
 como una extension de la primera.
-
-Un profesor es una persona dedicada a la docencia, tanto 
-en colegios publicos como en el ámbito privado.
-
-El código ha sido hecho por Carlos Hernandez en mayo del 2019
- */
+*/
 
 public class Persona {
     String numerodeTelefono;
