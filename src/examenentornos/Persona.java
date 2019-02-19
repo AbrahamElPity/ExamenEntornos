f0f3a63a31c09de9c964db0f8c61b70e9cd2e2e4
Alumno <Alumno@AULA1-PC09>
2019-02-19 12:24:48 +0100

    Ejercicio 2 documentacion clase Persona

--- src/examenentornos/Persona.java
+++ src/examenentornos/Persona.java
@@ -1,19 +1,29 @@
 package examenentornos;
 /*Este codigo define la clase Persona y la clase Profesor
 como una extension de la primera.
+*@author Abraham
+*Version 2.0
 */
 
 public class Persona {
     String numerodeTelefono;
+    /*El constructor
+    @param numerodeTelefono,por parametro le enviamos el numero del telefono
+    */
     public Persona(String numerodeTelefono){
         
     }
-
-    public String getNumerodeTelefono() {
+    /*
+    *@return numerodeTelefono
+    devuelve el numero de telefono  
+    */
+    public String obtenerTelefono() {
         return numerodeTelefono;
     }
-
-    public void setNumerodeTelefono(String numerodeTelefono) {
+    /*
+    @param numerodetelefono, sirve para poner el numero de telefono desde otras clases.
+    */
+    public void ponerTelefono(String numerodeTelefono) {
         this.numerodeTelefono = numerodeTelefono;
     }
             
