f005cf1904e93a301b6414439657271b8ed96cc6
Alumno <Alumno@AULA1-PC09>
2019-02-19 11:58:53 +0100

    Commit inicial clase Persona

---
+++ src/examenentornos/Persona.java
@@ -0,0 +1,27 @@
+package examenentornos;
+/*Este codigo define la clase Persona y la clase Profesor
+como una extension de la primera.
+
+Un profesor es una persona dedicada a la docencia, tanto 
+en colegios publicos como en el ámbito privado.
+
+El código ha sido hecho por Carlos Hernandez en mayo del 2019
+ */
+
+public class Persona {
+    String numerodeTelefono;
+    public Persona(String numerodeTelefono){
+        
+    }
+
+    public String getNumerodeTelefono() {
+        return numerodeTelefono;
+    }
+
+    public void setNumerodeTelefono(String numerodeTelefono) {
+        this.numerodeTelefono = numerodeTelefono;
+    }
+            
+            
+    
+}
