22b9daf7288a1d4d337b0344c2ac7de0044913a4
Alumno <Alumno@AULA1-PC09>
2019-02-19 12:19:41 +0100

    Ejercicio 2 documentacion

--- src/examenentornos/Profesor.java
+++ src/examenentornos/Profesor.java
@@ -1,25 +1,40 @@
 package examenentornos;
-
-import java.util.List;
+/**
+ * Es una clase la cual define a un profesor, esta clase hereda de la clase persona.
+ * @author Abraham Gómez
+ * Version 2.0
+ */
 
 public class Profesor extends Persona{
     String srt;
     int edad;
     String numerodeTelefono;
     List<Prestamo> prestamos;
+    /*El constructor
+    *@param numerodeTelefono
+    */
     public Profesor(String numerodeTelefono){
         super(numerodeTelefono);
     }
-    
+    /*
+    *Metodo que muestra la informacion personal.
+    *@return "Nombre: "+srt+"\nEdad: "+edad+"Telefono: "+this.numerodeTelefono;
+    */
     public String mostrarInfoPersonal(){
         return "Nombre: "+srt+"\nEdad: "+edad+"Telefono: "+this.numerodeTelefono;
     }
+    /*
+    *Muestra toda la informacion sobre el profesor.
+    */
     public void mostrarTodaInformacion(){
         System.out.println("Nombre: "+srt);
         System.out.println("Edad: "+edad);
         System.out.println("Telefono: "+this.numerodeTelefono);
         prestamo();
     }
+    /*
+    *Metodo que va mostrando los prestamos del profesor, este metodo es usado en el metodo superior.
+    */
     public void prestamo(){
         for(Prestamo p:prestamos){
             System.out.println(p);
